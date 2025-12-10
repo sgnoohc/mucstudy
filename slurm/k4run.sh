9cd39e1e327ec9cc1ffee93d3a1c63f26733b9e7
Kyrylo Sokolov <ksokolov@login12.ufhpc>
2025-12-10 16:52:37 -0500

    Add indentation to EOF

--- slurm/k4run.sh
+++ slurm/k4run.sh
@@ -11,10 +11,10 @@ set -euo pipefail
 pwd; hostname; date
 echo "started running muon collider simulation"
 apptainer exec /cvmfs/unpacked.cern.ch/ghcr.io/muoncollidersoft/mucoll-sim-alma9:latest bash << 'EOF'
-set -eo pipefail
-. /opt/spack/opt/spack/linux-almalinux9-x86_64/gcc-11.5.0/mucoll-stack-master-h2ssl2yh2yduqnhsv2i2zcjws74v7mcq/setup.sh # aliased by setup_mucoll
-. ./setup.sh
-k4run steering/reco_steer.py --InFileName MuMuToZH_sim.slcio --NEvents 10
+  set -eo pipefail
+  . /opt/spack/opt/spack/linux-almalinux9-x86_64/gcc-11.5.0/mucoll-stack-master-h2ssl2yh2yduqnhsv2i2zcjws74v7mcq/setup.sh # aliased by setup_mucoll
+  . ./setup.sh
+  k4run steering/reco_steer.py --InFileName MuMuToZH_sim.slcio --NEvents 10
 EOF
 echo "finished running muon collider simulation"
 date
